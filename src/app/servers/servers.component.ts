26396ab465ef2cef1bbba83b3e56869483d96451
oleg.bezkorovaynyi <oleg.bezkororvaynyi@globallogic.com>
2017-04-24 10:21:17 +0300

    data binding

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,19 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {setTimeout} from "timers";
 
 @Component({
   selector: 'app-servers',
   //selector: '[app-servers]',
   // selector: '.app-servers',
-  template: `
-    <app-server></app-server>
-    <app-server></app-server>`,
+  templateUrl: './servers.component.html',
+  // template: `
+  //   <app-server></app-server>
+  //   <app-server></app-server>`,
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
+  allowNewServer: boolean = false;
+  serverCreationStatus = 'No Servers Was Created!';
+  serverName = 'Test server';
+  username = '';
+  serverCreated = false;
+  servers = ['Test server', 'Test server 2'];
 
-  constructor() { }
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 3000);
+  }
 
   ngOnInit() {
+  }
+
+  onCreateServer() {
+    this.serverCreated = true;
+    this.servers.push(this.serverName);
+    this.serverCreationStatus = 'Server Was Created! Name is ' + this.serverName;
+  }
+
+  onResetUsername(event: Event) {
+    this.username = '';
+  }
+
+  onUpdateServerName(event: Event) {
+    this.serverName = (<HTMLInputElement>event.target).value;
   }
 
 }
